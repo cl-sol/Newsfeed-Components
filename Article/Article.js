99dbf2b0c808535caf0cbd3eeaa610953e56d18e
Claudia Sol <Claudia@Claudias-MacBook-Pro.local>
2020-04-14 16:53:17 -0400

    created elements in function

--- Article/Article.js
+++ Article/Article.js
@@ -112,3 +112,24 @@ const data = [
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new article.
 
 */
+
+/* Step 1 */
+
+/* <div class="article">
+    <h2>{title of the article}</h2>
+    <p class="date">{date of the article}</p>
+
+    {three separate paragraph elements}
+
+    <span class='expandButton'></span>
+  </div> */
+
+  function newArticle(title, date, firstParagrah, secondParagraph, thirdParagraph) {
+    const article = document.createElement("div");
+    const title = document.createElement("h2");
+    const date = document.createElement("p");
+    const paragraphOne = document.createElement("p");
+    const paragraphTwo = document.createElement("p");
+    const paragraphThree = document.createElement("p");
+
+  }
